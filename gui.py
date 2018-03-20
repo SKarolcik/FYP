b1cfc000051087e6e77dd473e158d95ae151e384
SKarolcik <s.karolcik@gmail.com>
2018-03-20 12:50:11 +0000

    Tested GUI with SPI handshake on RPi

--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@ __author__ = 'Stefan'
 import matplotlib
 matplotlib.use('TkAgg')
 import numpy as np
+import pigpio
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from Tkinter import *
@@ -17,19 +18,17 @@ class mclass:
     def __init__(self,  window):
         self.window = window
         self.box = Entry(window)
-        self.close_button = Button(window, text="Quit", command=window.quit)
+        self.close_button = Button(window, text="Quit", command=self.exitWindow)
         self.sendSPIbutton = Button(window,text="Send through SPI", command=self.sendSPI)
         self.readSPIlabel = Label(window, text="Received from SPI:")
         self.readSPIvalue = Label(window, text="N/A")
         self.terminalView = Frame(window, heigh=100,width=300)
         
-        self.checkbutton = Button (window, text="Check", command=self.plot)
         self.close_button.grid(row=2,column=4)
         self.box.grid(row=2,column=3)
         self.readSPIlabel.grid(row=3,column=2)
         self.readSPIvalue.grid(row=3,column=3)
         self.sendSPIbutton.grid(row=2,column=2)
-        self.checkbutton.grid(row=4,column=2)
         self.terminalView.grid(row=6,column=2,columnspan=3)
 
         self.pi = pigpio.pi()             # exit script if no connection
@@ -38,34 +37,38 @@ class mclass:
         self.pi.hardware_clock(4, 1000000) # 5 KHz clock on GPIO 4
 
 
-
+    def exitWindow(self):
+        self.pi.hardware_clock(4,0)
+        self.pi.stop()
+        self.window.quit()
+        
     def plot (self):
-        #x=np.array ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-        #v= np.array ([16,16.31925,17.6394,16.003,17.2861,17.3131,19.1259,18.9694,22.0003,22.81226])
-        #p= np.array ([16.23697,     17.31653,     17.22094,     17.68631,     17.73641 ,    18.6368,
-        #    19.32125,     19.31756 ,    21.20247  ,   22.41444   ,  22.11718  ,   22.12453])
     
         fig = Figure(figsize=(4,4))
         a = fig.add_subplot(111)
         a.imshow(heat_map, cmap='hot', interpolation='nearest')
-        #a.plot(p, range(2 +max(x)),color='blue')
-        #a.invert_yaxis()
 
         a.set_title ("Estimation Grid", fontsize=16)
         a.set_ylabel("Y", fontsize=14)
         a.set_xlabel("X", fontsize=14)
         
         canvas = FigureCanvasTkAgg(fig, master=self.window)
-        #canvas = prepHeatMap(heat_map)
         canvas.get_tk_widget().grid(row=5,column=2,columnspan=3)
-        #canvas.pack()
         canvas.draw()
 
     def sendSPI(self):
-        print "Nothing " + self.box.get()
-        h = self.pi.spi_open(1, 50000, 3) 
-        (count, rx_data) = pi.spi_xfer(h, b'\x01\x80\x00')
-        self.readSPIvalue["text"] = self.box.get()
+        hex_vals = self.box.get()
+        self.box.delete(0,END)
+        print "Nothing " + hex_vals
+        hex_data = bytearray.fromhex(hex_vals)
+        h = self.pi.spi_open(0, 50000, 0b0100000011110000000001) 
+        (count, rx_data) = self.pi.spi_xfer(h, hex_data)
+        print count
+        print rx_data[0]
+        self.readSPIvalue["text"] = str(hex(rx_data[0])) + " " + str(hex(rx_data[1]))
+        if (hex_data[1] == 0x80):
+            self.plot()
+            
 
 
 
