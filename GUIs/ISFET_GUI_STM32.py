21397181bb0d4fe06b6321fa624941010ba24850
SKarolcik <s.karolcik@gmail.com>
2019-07-22 18:23:10 +0000

    PG calibration implemented

--- GUIs/ISFET_GUI_STM32.py
+++ GUIs/ISFET_GUI_STM32.py
@@ -14,145 +14,11 @@ import struct
 from matplotlib import cm
 from pyqtgraph.Qt import QtCore, QtGui
 import pyqtgraph as pg
+import coloredGraph
 
 FREQUENCY = 2000000
 
 #avconv -f x11grab -r 25 -s 1920x1080 -i :0.0+0,0 -vcodec libx264 video.mkv
-
-def cmapToColormap(cmap, nTicks=16):
-    """
-    Converts a Matplotlib cmap to pyqtgraphs colormaps. No dependency on matplotlib.
-
-    Parameters:
-    *cmap*: Cmap object. Imported from matplotlib.cm.*
-    *nTicks*: Number of ticks to create when dict of functions is used. Otherwise unused.
-    """
-
-    # Case #1: a dictionary with 'red'/'green'/'blue' values as list of ranges (e.g. 'jet')
-    # The parameter 'cmap' is a 'matplotlib.colors.LinearSegmentedColormap' instance ...
-    if hasattr(cmap, '_segmentdata'):
-        colordata = getattr(cmap, '_segmentdata')
-        if ('red' in colordata) and isinstance(colordata['red'], collections.Sequence):
-            # print("[cmapToColormap] RGB dicts with ranges")
-
-            # collect the color ranges from all channels into one dict to get unique indices
-            posDict = {}
-            for idx, channel in enumerate(('red', 'green', 'blue')):
-                for colorRange in colordata[channel]:
-                    posDict.setdefault(colorRange[0], [-1, -1, -1])[idx] = colorRange[2]
-
-            indexList = list(posDict.keys())
-            indexList.sort()
-            # interpolate missing values (== -1)
-            for channel in range(3):  # R,G,B
-                startIdx = indexList[0]
-                emptyIdx = []
-                for curIdx in indexList:
-                    if posDict[curIdx][channel] == -1:
-                        emptyIdx.append(curIdx)
-                    elif curIdx != indexList[0]:
-                        for eIdx in emptyIdx:
-                            rPos = (eIdx - startIdx) / (curIdx - startIdx)
-                            vStart = posDict[startIdx][channel]
-                            vRange = (posDict[curIdx][channel] - posDict[startIdx][channel])
-                            posDict[eIdx][channel] = rPos * vRange + vStart
-                        startIdx = curIdx
-                        del emptyIdx[:]
-            for channel in range(3):  # R,G,B
-                for curIdx in indexList:
-                    posDict[curIdx][channel] *= 255
-
-            posList = [[i, posDict[i]] for i in indexList]
-            return posList
-
-        # Case #2: a dictionary with 'red'/'green'/'blue' values as functions (e.g. 'gnuplot')
-        elif ('red' in colordata) and isinstance(colordata['red'], collections.Callable):
-            # print("[cmapToColormap] RGB dict with functions")
-            indices = np.linspace(0., 1., nTicks)
-            luts = [np.clip(np.array(colordata[rgb](indices), dtype=np.float), 0, 1) * 255 \
-                    for rgb in ('red', 'green', 'blue')]
-            return list(zip(indices, list(zip(*luts))))
-
-    # If the parameter 'cmap' is a 'matplotlib.colors.ListedColormap' instance, with the attributes 'colors' and 'N'
-    elif hasattr(cmap, 'colors') and hasattr(cmap, 'N'):
-        colordata = getattr(cmap, 'colors')
-        # Case #3: a list with RGB values (e.g. 'seismic')
-        if len(colordata[0]) == 3:
-            # print("[cmapToColormap] list with RGB values")
-            indices = np.linspace(0., 1., len(colordata))
-            scaledRgbTuples = [(rgbTuple[0] * 255, rgbTuple[1] * 255, rgbTuple[2] * 255) for rgbTuple in colordata]
-            return list(zip(indices, scaledRgbTuples))
-
-        # Case #3: a list of tuples with positions and RGB-values (e.g. 'terrain')
-        # -> this section is probably not needed anymore!?
-        elif len(colordata[0]) == 2:
-            # print("[cmapToColormap] list with positions and RGB-values. Just scale the values.")
-            scaledCmap = [(idx, (vals[0] * 255, vals[1] * 255, vals[2] * 255)) for idx, vals in colordata]
-            return scaledCmap
-
-    # Case #X: unknown format or datatype was the wrong object type
-    else:
-        raise ValueError("[cmapToColormap] Unknown cmap format or not a cmap!")
-
-
-class MplCmapImageView(pg.ImageView):
-    def __init__(self, additionalCmaps=[], setColormap=None, **kargs):
-        super(MplCmapImageView, self).__init__(**kargs)
-
-        self.gradientEditorItem = self.ui.histogram.item.gradient
-
-        self.activeCm = "grey"
-        self.mplCmaps = {}
-
-        if len(additionalCmaps) > 0:
-            self.registerCmap(additionalCmaps)
-
-        if setColormap is not None:
-            self.gradientEditorItem.restoreState(setColormap)
-
-
-
-    def registerCmap(self, cmapNames):
-        """ Add matplotlib cmaps to the GradientEditors context menu"""
-        self.gradientEditorItem.menu.addSeparator()
-        savedLength = self.gradientEditorItem.length
-        self.gradientEditorItem.length = 100
-
-        # iterate over the list of cmap names and check if they're avaible in MPL
-        for cmapName in cmapNames:
-            if not hasattr(cm, cmapName):
-                print('[extendedimageview] Unknown cmap name: \'{}\'. Your Matplotlib installation might be outdated.'.format(cmapName))
-            else:
-                # create a Dictionary just as the one at the top of GradientEditorItem.py
-                cmap = getattr(cm, cmapName)
-                self.mplCmaps[cmapName] = {'ticks': cmapToColormap(cmap), 'mode': 'rgb'}
-
-                # Create the menu entries
-                # The following code is copied from pyqtgraph.ImageView.__init__() ...
-                px = QtGui.QPixmap(100, 15)
-                p = QtGui.QPainter(px)
-                self.gradientEditorItem.restoreState(self.mplCmaps[cmapName])
-                grad = self.gradientEditorItem.getGradient()
-                brush = QtGui.QBrush(grad)
-                p.fillRect(QtCore.QRect(0, 0, 100, 15), brush)
-                p.end()
-                label = QtGui.QLabel()
-                label.setPixmap(px)
-                label.setContentsMargins(1, 1, 1, 1)
-                act = QtGui.QWidgetAction(self.gradientEditorItem)
-                act.setDefaultWidget(label)
-                act.triggered.connect(self.cmapClicked)
-                act.name = cmapName
-                self.gradientEditorItem.menu.addAction(act)
-        self.gradientEditorItem.length = savedLength
-
-
-    def cmapClicked(self, b=None):
-        """onclick handler for our custom entries in the GradientEditorItem's context menu"""
-        act = self.sender()
-        self.gradientEditorItem.restoreState(self.mplCmaps[act.name])
-        self.activeCm = act.name
-
 
 class GuiViewer(QtGui.QWidget):
 
@@ -192,7 +58,7 @@ class GuiViewer(QtGui.QWidget):
         self.timeLb = QtGui.QLabel('t = N/A')
 
         #self.imv = pg.ImageView()
-        self.imv = MplCmapImageView(additionalCmaps=['jet', 'viridis', 'seismic', 'cubehelix'], view=pg.PlotItem())
+        self.imv = coloredGraph.MplCmapImageView(additionalCmaps=['jet', 'viridis', 'seismic', 'cubehelix'], view=pg.PlotItem())
         self.imv.setLevels(0,250)
         tmp_frame = np.transpose(np.zeros((64,200)))
         self.imv.setImage(tmp_frame, autoRange=False, autoLevels=False, autoHistogramRange=False)
@@ -270,6 +136,10 @@ class GuiViewer(QtGui.QWidget):
         self.reference_f = 0
         self.frame = 0
         self.setRef = 0
+
+        self.outFile = open("isfet_frames.dat", "w")
+        self.thread = ThreadedTask(self.outFile, self.thread_queue)
+        self.thread.start()
 
         self.received_frame = 0
         self.PG_f = 0
@@ -364,6 +234,10 @@ class GuiViewer(QtGui.QWidget):
     def quitWindow(self):
         self.pi.hardware_clock(4,0)
         self.pi.stop()
+        if self.thread != 0:
+            self.thread.stop() 
+            self.thread.join()
+        self.outFile.close()
         self.close()
 
     def isr_frame (self, gpio, level, tick):
@@ -378,7 +252,8 @@ class GuiViewer(QtGui.QWidget):
         frame_conv = np.array(map(lambda x : ((2.0 - (x/4095.0)*3.25)/8.0)*1000, frame_values))
         self.received_frame = frame_conv
         #print ("Real frame: " + str(frame_conv[0]) + " " + str(frame_conv[1]) + " " + str(frame_conv[2]) + " " + str(frame_conv[3]) + " " + str(frame_conv[4]))
-        self.frame = np.transpose(frame_conv.reshape((64,200)))
+        new_frame = np.transpose(frame_conv.reshape((64,200)))
+        self.frame = new_frame
         #print self.frame[120][1]
         avgDat = np.mean(frame_conv)
         #condition = (frame_conv>=60) & (frame_conv<=180)
@@ -396,7 +271,15 @@ class GuiViewer(QtGui.QWidget):
         #print "Interrupt happened\n"
         self.frames_el += 1
         self.time_el = time.time() - self.start_time
-        self.queue.put((self.frame,self.frames_el, self.time_el, self.avgValVect))
+        if self.saveToFile.isChecked():
+            self.thread_queue.put((self.frames_el, new_frame, avgDat, self.time_el))
+            if (self.frames_el % 10 == 0):
+                self.queue.put((new_frame,self.frames_el, self.time_el, self.avgValVect))
+        else:
+            self.queue.put((new_frame,self.frames_el, self.time_el, self.avgValVect))
+                
+
+        #self.queue.put((new_frame,self.frames_el, self.time_el, self.avgValVect))
 
     def resetChip(self): 
         (count, rx_data) = self.pi.spi_xfer(self.SPIhandle, [0, 0])
@@ -447,6 +330,24 @@ class GuiViewer(QtGui.QWidget):
                 self.polling_started = 1
                 self.pollQueue()
 
+class ThreadedTask(threading.Thread):
+    def __init__(self, outFile, queue):
+        threading.Thread.__init__(self)
+        self.outFile = outFile
+        self.queue = queue
+        self._stop_event = threading.Event()
+    def stop(self):
+        print "Stopping writing to file"
+        self._stop_event.set()
+    def stopped(self):
+        return self._stop_event.is_set()
+    def run(self):   
+        while not self.stopped():
+            if not self.queue.empty():
+                frame_struct = self.queue.get()
+                self.outFile.write("Frame no: " + str(frame_struct[0]) + "; Time elapsed = " + str(frame_struct[3]) + "s\n")
+                np.savetxt(self.outFile, frame_struct[1], fmt='%.4f', delimiter=' ', newline='\n')
+                self.outFile.write("Average values of frame: " + str(frame_struct[2]) + "\n\n")
 
 def main():
 
